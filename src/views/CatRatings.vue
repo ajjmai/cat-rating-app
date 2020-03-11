ed86c913f472be976b62ed5b06cf641dde0a54dc
Aija <aija.valleala@gmail.com>
2020-03-11 19:27:20 +0200

    create cat-ratings page

---
+++ src/views/CatRatings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>Cat ratings</div>
+    <div>Kissa komponentti</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CatRatings"
+};
+</script>
+
+<style scoped>
+</style>
